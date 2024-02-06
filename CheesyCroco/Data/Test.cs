45dffcd5e2198895b19946d610a5b9d4375b0050
Ekats3529 <45201820+Ekats3529@users.noreply.github.com>
2024-02-06 15:43:16 +0400

    Добавьте файлы проекта.

---
+++ CheesyCroco/Data/Test.cs
@@ -0,0 +1,11 @@
+﻿namespace CheesyCroco.Data
+{
+    public class Test
+    {
+        public string Name { get; set; }
+
+        public float Rate { get; set; }
+
+        public int PassCounter { get; set; }
+    }
+}
---
+++ CheesyCroco/Data/TestService.cs
@@ -0,0 +1,16 @@
+﻿namespace CheesyCroco.Data
+{
+    public class TestService
+    {
+
+        public Task<Test[]> GetTestAsync()
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Test
+            {
+                Name = "Aaa",
+                Rate = 5,
+                PassCounter = 100000000
+            }).ToArray());
+        }
+    }
+}
