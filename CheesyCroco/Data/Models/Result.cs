205a28a3bc66bf4d4f18b8d685b0ab9fcff116ef
Ekats3529 <45201820+Ekats3529@users.noreply.github.com>
2024-03-29 19:34:58 +0400

    Result changes

--- CheesyCroco/Data/Models/Result.cs
+++ CheesyCroco/Data/Models/Result.cs
@@ -8,14 +8,18 @@ namespace CheesyCroco.Data.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
-
-        public string testId { get; set; } = ObjectId.GenerateNewId().ToString();
+        
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string testId { get; set; } = "";
 
         public string name { get; set; } = "";
 
         public string text { get; set; } = "";
 
-        public int scoreLimit { get; set; } = 0;
+        public int scoreMin { get; set; } = 0;
+
+        public int scoreMax { get; set; } = 0;
+
         public int userCount { get; set; } = 0;
     }
 }
